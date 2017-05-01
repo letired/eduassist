40b1d2fa7eae13740884be3ff67766fee9bc54a2
Spencer Bray <spencerjohnbray@gmail.com>
2017-05-01 15:16:04 +0200

    first three routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'classes#index'
   devise_for :users
+  resources :classes
+  resources :students
+  # resources :assignments
+  # resources :grades, only: [ :create, :update, :destroy ]
+  # resources :attendances,
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
