c3e8f79b55f4b8307d0dde185fa62e5ba04d187e
Spencer Bray <spencerjohnbray@gmail.com>
2017-05-01 14:02:41 +0200

    add first_name, last_name, admin to user

---
+++ db/migrate/20170501115920_add_name_and_admin_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameAndAdminToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :admin, :boolean, default: false, null: false
+  end
+end
