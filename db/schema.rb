383391ed1939bac2880cf9e87f28af6b0acd4e20
Spencer Bray <spencerjohnbray@gmail.com>
2017-05-01 14:09:56 +0200

    add student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501120721) do
+ActiveRecord::Schema.define(version: 20170501120917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20170501120721) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_school_classes_on_user_id", using: :btree
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "bio"
+    t.date     "birthday"
+    t.integer  "school_class_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["school_class_id"], name: "index_students_on_school_class_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +56,5 @@ ActiveRecord::Schema.define(version: 20170501120721) do
   end
 
   add_foreign_key "school_classes", "users"
+  add_foreign_key "students", "school_classes"
 end
