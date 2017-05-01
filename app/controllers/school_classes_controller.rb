eb272bedb0b841a034958caeb95aaf1b29352b41
Patrick Scheuchzer <scheuchzer.patrick@gmail.com>
2017-05-01 16:44:25 +0200

    school_classes#new & #create

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,9 +6,18 @@ class SchoolClassesController < ApplicationController
   end
 
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
+    @user = User.find(current_user.id)
+    @school_class = SchoolClass.new(new_school_class_params)
+    @school_class.user = @user
+    if @school_class.save
+      redirect_to school_class_path(@school_class)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +27,11 @@ class SchoolClassesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def new_school_class_params
+    params.require(:school_class).permit( :name, :description )
   end
 end
