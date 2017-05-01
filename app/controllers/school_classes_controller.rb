3a2a2f4f1e2bb871d09e717219068805dfedf01d
Pauline Gaget <lupiane@hotmail.com>
2017-05-01 15:51:29 +0200

    skeleton schoolclasses controller

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,39 @@
+class SchoolClassesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(new_class_params)
+    @school_class.user = User.find(params[:user_id])
+    @school_class.save
+    redirect_to "school_classes/index"
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+
+  def new_class_params
+    params.require(:school_class).permit(:name, :description)
+  end
+end
