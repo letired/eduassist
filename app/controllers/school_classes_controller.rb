3bfa52f581c5bdaa51cbcf712f40d5d9e8ec98a8
Patrick Scheuchzer <scheuchzer.patrick@gmail.com>
2017-05-02 16:46:54 +0200

    created all SchoolClasses actions and respective views

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -15,30 +15,38 @@ class SchoolClassesController < ApplicationController
 
   def create
     @user = User.find(current_user.id)
-    @school_class = SchoolClass.new(new_school_class_params)
+    @school_class = SchoolClass.new(school_class_params)
     @school_class.user = @user
     if @school_class.save
-      redirect_to school_class_path(@school_class)
+      redirect_to new_school_class_student_path(@school_class), notice: "School Class was created successfully. Let's add the first student."
     else
       render :new
     end
   end
 
   def edit
-    
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
-
+    @school_class = SchoolClass.find(params[:id])
+    @user = User.find(current_user.id)
+    if @school_class.update(school_class_params)
+      redirect_to school_class_path(@school_class), notice: 'School Class was updated successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    school_class = SchoolClass.find(params[:id])
+    school_class.destroy
+    redirect_to school_classes_path, notice: 'School Class was deleted successfully.'
   end
 
   private
 
-  def new_school_class_params
+  def school_class_params
     params.require(:school_class).permit( :name, :description )
   end
 end
