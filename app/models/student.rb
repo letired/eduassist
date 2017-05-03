8e18f341d245a4a08c6e83cc307078c7bd9be025
Patrick Scheuchzer <patrick-scheuchzer@users.noreply.github.com>
2017-05-03 12:06:11 +0200

    removed unnecessary line

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :school_class
-  delegate :user, to: :school_class, allow_nil: true
   has_many :attendances, dependent: :destroy
   has_many :grades, dependent: :destroy
   has_many :assignments, through: :grades
