4701c0d4482fa60109eb20edd4d304fbc555ee13
Pauline Gaget <lupiane@hotmail.com>
2017-05-09 19:59:16 +0200

    reference to struggling students added to class show page

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -2,7 +2,7 @@ module GradesHelper
   def show_student_average(student)
     name = h(student.first_name + ' ' + student.last_name)
     average = student.average.round(2).to_s + " %"
-    return average.to_s + " " + name
+    return '<i class="fa fa-user" aria-hidden="true"></i>'.html_safe + "#{name} (average: #{average.to_s})"
   end
 
   def show_three_lowest_scoring(school_class)
