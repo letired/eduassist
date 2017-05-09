0b6b1a9417b3b618098879299a95ac6e21173328
Pasquale Kress <wabisabisf@gmail.com>
2017-05-09 17:12:46 +0200

    moved the average score into the student model

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -1,25 +1,19 @@
 module GradesHelper
   def show_student_average(student)
-
-    earned_points = 0
-    student.grades.each do |grade|
-      earned_points += grade.earned_points
-    end
-
-    possible_points = 0
-    student.assignments.each do |assignment|
-      possible_points += assignment.max_points
-    end
-
-    average = ((earned_points / possible_points.to_f) * 100).round(2).to_s + " %"
+    name = h(student.first_name + ' ' + student.last_name)
+    average = student.average.round(2).to_s + " %"
+    return average.to_s + " " + name
   end
 
   def show_three_lowest_scoring(school_class)
+
     scores = []
     school_class.students.each do |student|
       scores << show_student_average(student)
     end
-    scores.sort.first(3)
+
+    scores = scores.sort
+    return scores.first(3).join(tag(:br)).html_safe
   end
 
   def show_assignments_missed(school_class)
