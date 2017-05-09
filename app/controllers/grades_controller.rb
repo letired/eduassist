2c18f3fa026a6cf14f97cc20a964a1535a54c5fc
Pasquale Kress <wabisabisf@gmail.com>
2017-05-09 12:25:02 +0200

    summary now shows percentages

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -14,33 +14,38 @@ class GradesController < ApplicationController
   def stats
     @school_class = policy_scope(SchoolClass).find(params[:id])
 
-
     @assignments = Assignment.where(school_class_id: @school_class).order(date: :asc)
 
     @categories = []
     @answer = {}
 
-     @assignments.each do |ass|
+    @assignments.each do |ass|
        @categories <<  ass.category
-
     end
+
     @categories = @categories.uniq!
 
     @categories.each do |cat|
       points = 0
       amount = 0
+      maxPoints = 0
+      amount2 = 0
       ass2 = @assignments.where(category: cat)
       ass2.each do |as2|
-           as2.grades.each do |gra|
-                points += gra.earned_points
-                amount += 1
-              end
-           end
-       ave = points / amount
-       @answer[cat] = ave
+        as2.grades.each do |gra|
+          points += gra.earned_points
+          amount += 1
+        end
       end
-
-
+      ass3 = @assignments.where(category: cat)
+      ass3.each do |as3|
+        maxPoints += as3.max_points
+        amount2 += 1
+      end
+      allPoints = (maxPoints/amount2).to_f
+      av = ((points / amount) / allPoints * 100).floor
+      @answer[cat] = av
+    end
 
     @grades = Grade.where(school_class_id: @school_class).order(date: :asc)
     rescue ActiveRecord::RecordNotFound
