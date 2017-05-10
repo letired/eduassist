8e3c80d974ca845d6fe95d71b451e2e994ef254a
Pauline Gaget <lupiane@hotmail.com>
2017-05-10 11:20:05 +0200

    now with links to individual student grades

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -2,7 +2,7 @@ module GradesHelper
   def show_student_average(student)
     name = h(student.first_name + ' ' + student.last_name)
     average = student.average.round(2).to_s + " %"
-    return '<i class="fa fa-user" aria-hidden="true"></i>'.html_safe + "#{name} (average: #{average.to_s})"
+    return '<i class="fa fa-user" aria-hidden="true"></i><a href="'.html_safe + "/students/#{student.id}/assignments" + '">  '.html_safe + "#{name} (average: #{average.to_s})" + '</a>'.html_safe
   end
 
   def show_three_lowest_scoring(school_class)
