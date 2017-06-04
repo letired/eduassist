c0d6bd8a632cd63fb6d1512b7cb777f1d9e31fcb
Pasquale Kress <wabisabisf@gmail.com>
2017-06-04 20:14:42 +0200

    added a pop up notice to help out firefox users

---
+++ app/helpers/detect_browser_helper.rb
@@ -0,0 +1,18 @@
+module DetectBrowserHelper
+  def client_browser_name
+    user_agent = request.env['HTTP_USER_AGENT'].downcase
+    if user_agent =~ /msie/i
+      "Internet Explorer"
+    elsif user_agent =~ /konqueror/i
+      "Konqueror"
+    elsif user_agent =~ /gecko/i
+      "Mozilla"
+    elsif user_agent =~ /opera/i
+      "Opera"
+    elsif user_agent =~ /applewebkit/i
+      "Safari"
+    else
+      "Unknown"
+    end
+  end
+end
